a9a0b7d860667c53b1080079eef195291c75df01
Aditya_prajapati <114338023+prajapati-aditya@users.noreply.github.com>
2025-09-15 23:54:32 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (42.37%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -4,27 +4,26 @@ class Solution:
             return []
 
         # mapping digits to letters like phone keypad
-        mapping = {
+        dict = {
             "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
             "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
         }
 
         result = []
 
-        # helper function for backtracking
+        # backtracking
         def build(index: int, current: list[str]):
-            # if we've used all digits → form a word and store it
             if index == len(digits):
                 result.append("".join(current))
                 return
 
-            # try every possible letter for the current digit
-            for letter in mapping[digits[index]]:
-                current.append(letter)          # choose a letter
-                build(index + 1, current)       # go to next digit
-                current.pop()                   # undo choice (backtrack)
+            
+            for letter in dict[digits[index]]:
+                current.append(letter)          # do
+                build(index + 1, current)       # explore
+                current.pop()                   # undo or backtrack
 
-        # start from first digit with empty path
+        
         build(0, [])
         return result
         
