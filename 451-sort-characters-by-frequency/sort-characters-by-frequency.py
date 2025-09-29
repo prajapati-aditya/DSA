bd78333efcdc8da27cfa9b74763f7edf534545ff
Aditya_prajapati <114338023+prajapati-aditya@users.noreply.github.com>
2025-09-29 21:28:00 +0530

    Time: 4 ms (89.16%) | Memory: 19.1 MB (47.04%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -4,8 +4,8 @@ class Solution:
         dict1 = Counter (s)
         
         sorted_char = sorted(dict1.items(), key = lambda x : x[1], reverse =True )
-        res = [ ]
+        res = ""
         for ch , count in sorted_char :
-            res.append(ch*count)
-        return "".join(res)
+            res = res+(ch*count)
+        return res
         
