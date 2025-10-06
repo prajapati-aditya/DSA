89772718755d23c400906c36332e05431e070df5
Aditya_prajapati <114338023+prajapati-aditya@users.noreply.github.com>
2025-10-06 22:45:32 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (53.87%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -6,6 +6,7 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        '''
         res = []
         def dfs (node) :
             if not node :
@@ -15,5 +16,18 @@ class Solution:
             dfs(node.right)
         dfs(root)
         return res
+        '''
+        # using stack :
+        if not root :       # if there is null
+            return []
+        stack = [root]
+        res= []
+        while stack :
+            node = stack.pop()
+            res.append(node.val)
 
-        
+            if node.right:
+                stack.append(node.right) 
+            if node.left :
+                stack.append(node.left)
+        return res
