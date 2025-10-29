8c5c13e485a3bbfd7a288fb1c2d05470f423bf39
Aditya_prajapati <114338023+prajapati-aditya@users.noreply.github.com>
2025-10-29 10:58:40 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (26.92%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.py
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.py
@@ -1,4 +1,23 @@
 class Solution:
     def smallestNumber(self, n: int) -> int:
-        return (1 << (n.bit_length())) -1
+        bits = {
+            1: 1,
+            2: 3,
+            4: 7,
+            8: 15,
+            16: 31,
+            32: 63,
+            64: 127,
+            128: 255,
+            256: 511,
+            512: 1023,
+            1024: 2047,
+        }
+
+        prev =1 
+        for bit in bits.keys() :
+            if bit > n :
+                return bits[prev]
+            prev = bit
+        return bits[1024]
         
