cacee884936673dd9bcc9afafa18f47a0bd584e4
Aditya_prajapati <114338023+prajapati-aditya@users.noreply.github.com>
2025-11-10 12:22:38 +0530

    Time: 1 ms (15.45%) | Memory: 17.7 MB (53.92%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -2,15 +2,25 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        yes I'll do, don't you worry
         """
-        buckets=[0,0,0]
-        for num in nums :
-            buckets[num]+=1
-        
-        i=0
-        for num in range(3):
-            for _ in range(buckets[num]):
-                nums[i]=num
-                i+=1
-                
+        # DNF
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+        while mid <= high :
+            if nums[mid] == 0 :
+                nums[low] , nums[mid] = nums[mid] , nums[low]
+                low += 1
+                mid += 1
+            
+            elif nums[mid] == 1 :
+                mid += 1 
+            
+            else :    # means other than 0 and 1 is 2
+                nums[mid] , nums[high] = nums[high] , nums[mid]
+                high -= 1
         return nums
+
+
+        
